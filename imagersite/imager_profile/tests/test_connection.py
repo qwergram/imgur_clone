62066e893a32d121b53f408ee5d1faf71b8c6142
Norton Pengra <npengra317@gmail.com>
2016-04-11 14:35:10 -0700

    Created tests for profile creation

--- imagersite/imager_profile/tests/test_connection.py
+++ imagersite/imager_profile/tests/test_connection.py
@@ -16,12 +16,25 @@ class UserTestCase(TestCase):
         )
         self.user.set_password('pulse_rifle')
         self.user.save()
+        self.profile = models.ImagerProfile.objects.all()[0]
 
     def test_user_created(self):
         self.assertEquals(self.user.username, 'warlock')
 
-    def test_imgur_object_created(self):
+    def test_profile_created(self):
         self.assertTrue(models.ImagerProfile.objects.all())
 
-    def test_imgur_attachement_created(self):
+    def test_user_to_profile_connection_created(self):
         self.assertTrue(hasattr(self.user, 'profile'))
+
+    def test_profile_to_user_connection_created(self):
+        self.assertTrue(hasattr(self.profile, 'user'))
+
+    def test_user_to_profile_connection_is_correct(self):
+        self.assertEqual(self.user.profile, self.profile)
+
+    def test_profile_to_user_connection_is_correct(self):
+        self.assertEqual(self.profile.user, self.user)
+
+    def test_profile_string(self):
+        self.assertEqual(str(self.profile), 'warlock')
