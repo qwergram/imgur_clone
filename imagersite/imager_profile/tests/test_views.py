13f47d764ac05a11d3836f88f7e553969bf486a1
Norton Pengra <npengra317@gmail.com>
2016-04-12 22:29:15 -0700

    Tests for registration submition

--- imagersite/imager_profile/tests/test_views.py
+++ imagersite/imager_profile/tests/test_views.py
@@ -124,16 +124,25 @@ class RegisterPageViewTestCase(TestCase):
         })
         self.assertEqual(response.status_code, 200)
 
-    # def test_register_email_created(self):
-    #     response = self.client.post('/accounts/register/', {
-    #         "username": "kent",
-    #         "email": "kent@hiscompany.com",
-    #         "password1": "wabl",
-    #         "password2": "wabl"
-    #     })
-    #     import pdb; pdb.set_trace()
-    #
-    #     self.assertEqual(len(mail.outbox), 1)
+    def test_register_form_submit_works(self):
+        response = self.client.post('/accounts/register/', {
+            "username": "kent",
+            "email": "kent@hiscompany.com",
+            "password1": "wablwabl3",
+            "password2": "wablwabl3"
+        })
+        self.assertFalse('<form ' in response.content.decode())
+
+    def test_register_short_password(self):
+        response = self.client.post('/accounts/register/', {
+            "username": "kent",
+            "email": "kent@hiscompany.com",
+            "password1": "wabl",
+            "password2": "wabl"
+        })
+        self.assertTrue('<form ' in response.content.decode())
+
+        # self.assertEqual(len(mail.outbox), 1)
 
 
 class IndexPageDefaultViewTestCase(TestCase):
