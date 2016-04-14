ca143bb57b33fa1d5eacfe271cd1c16c691b1d21
Norton Pengra <npengra317@gmail.com>
2016-04-13 20:30:51 -0700

    Argument Error fixed

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -4,7 +4,7 @@ from .models import Album, Photo, PUBLIC
 # Create your views here.
 
 def latest_library_view(request, **kwargs):
-    return render(request, "library_view.html", {"photos": Photo.objects.filter(public=PUBLIC).order_by("-date_uploaded")[:100]})
+    return render(request, "library_view.html", {"photos": Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")[:100]})
 
 def album_view(request, album_id=None, **kwargs):
     return render(request, "album_base.html", {})
