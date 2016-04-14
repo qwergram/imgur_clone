ab24604e201542b91c30064f7ecf7ff2acc93028
Norton Pengra <npengra317@gmail.com>
2016-04-13 19:35:22 -0700

    Created skeleton for image views

---
+++ imagersite/imager_images/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.test import TestCase, Client
+from django.core import mail
+from imager_profile.tests.test_model import UserFactory
+from .tests.test_photos import PhotoFactory, AlbumFactory
+
+class LibraryImageTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get('/images/library/')
+
+
+class AlbumViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get('/images/album/')
+
+
+class PhotoViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get('/images/photos/')
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def latest_library_view(request, **kwargs):
+    pass
+
+def album_view(request, album_id=None, **kwargs):
+    pass
+
+def photo_view(request, photo_id=None, **kwrags):
+    pass
