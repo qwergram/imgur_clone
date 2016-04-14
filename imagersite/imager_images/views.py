1b7d0651431ab94b7db784ee696c5c2d9c20798d
Kent Ross <root.main@gmail.com>
2016-04-14 15:50:40 -0700

    photo page tests

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Album, Photo, PUBLIC
-
-# Create your views here.
+# coding=utf-8
+from django.shortcuts import render, get_object_or_404, Http404
+from .models import Album, Photo, PUBLIC, PRIVATE
 
 
 def latest_library_view(request, **kwargs):
-    return render(request, "library_view.html", {"photos": Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")[:100]})
+    return render(
+        request,
+        "library_view.html",
+        {"photos": Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")[:100]}
+    )
 
 
 def album_view(request, album_id=None, **kwargs):
@@ -15,4 +18,6 @@ def album_view(request, album_id=None, **kwargs):
 
 def photo_view(request, photo_id=None, **kwrags):
     photo = get_object_or_404(Photo, id=int(photo_id))
+    if photo.published == PRIVATE and photo.owner is not request.user:
+        raise Http404
     return render(request, "photo_view.html", {"photo": photo})
