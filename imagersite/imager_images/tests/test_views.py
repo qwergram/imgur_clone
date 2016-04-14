c271bf52712f6c067012a7b6d5a03ee3841be2f3
Norton Pengra <npengra317@gmail.com>
2016-04-13 20:56:00 -0700

    Added tests for photo views

--- imagersite/imager_images/tests/test_views.py
+++ imagersite/imager_images/tests/test_views.py
@@ -7,6 +7,14 @@ class LibraryImageTestCase(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.user = UserFactory.create()
+        self.photo = PhotoFactory.create(
+            owner=self.user.profile
+        )
+        self.private_photo = PhotoFactory.create(
+            owner=self.user.profile,
+            published="CLOSED"
+        )
         self.response = self.client.get('/images/library/')
 
     def test_view_exists(self):
@@ -14,6 +22,13 @@ class LibraryImageTestCase(TestCase):
 
     def test_view_is_correct(self):
         self.assertTrue("<h1>YOU ARE AT LIBRARY VIEW!</h1>" in self.response.content.decode())
+
+    def view_contains_images(self):
+        self.assertTrue('<article class="thumb">' in self.response.content.decode())
+
+    def view_contains_images_except_private(self):
+        self.assertFalse(self.private_photo.title in self.response.content.decode())
+
 
 class AlbumViewTestCase(TestCase):
 
