0aca183eaf1d28ca3636be444ec88ffe6106e725
Norton Pengra <npengra317@gmail.com>
2016-04-13 18:34:42 -0700

    added image to context_data

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from imager_images.models import Photo
+from imager_images.models import Photo, PUBLIC
 
 # Create your views here.
 
@@ -9,5 +9,5 @@ class IndexView(TemplateView):
 
     def get_context_data(self):
         return {
-            random_photo: Photo.objects.filter(published=Photo.PUBLIC).order_by("-date_uploaded")[:100].order_by("?")[0]
+            "random_photo": Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded").order_by("?")[0:1]
         }
