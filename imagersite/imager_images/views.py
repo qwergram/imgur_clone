9dd4463198c5423b07b9c732513eeec9ec642b4b
Norton Pengra <npengra317@gmail.com>
2016-04-13 20:28:08 -0700

    Filter the images to only show publicly shared ones

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Album, Photo
+from .models import Album, Photo, PUBLIC
 
 # Create your views here.
 
 def latest_library_view(request, **kwargs):
-    return render(request, "library_view.html", {"photos": Photo.objects.order_by("-date_uploaded")[:100]})
+    return render(request, "library_view.html", {"photos": Photo.objects.filter(public=PUBLIC).order_by("-date_uploaded")[:100]})
 
 def album_view(request, album_id=None, **kwargs):
     return render(request, "album_base.html", {})
