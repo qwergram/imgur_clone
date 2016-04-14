2975c2660200b9cd9c1dcd62873711fca625b314
Norton Pengra <npengra317@gmail.com>
2016-04-13 20:42:31 -0700

    Created view for single photo with url handling

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Album, Photo, PUBLIC
 
 # Create your views here.
@@ -10,4 +10,8 @@ def album_view(request, album_id=None, **kwargs):
     return render(request, "album_base.html", {})
 
 def photo_view(request, photo_id=None, **kwrags):
-    return render(request, "album_base.html", {})
+    if photo_id:
+        photo = get_object_or_404(Photo, id=photo_id)
+    else:
+        return redirect('library')
+    return render(request, "photo_view.html", {"photo": photo})
