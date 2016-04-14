6209431b6586ffb4ec7bdb8a69e442461a0ea770
Kent Ross <root.main@gmail.com>
2016-04-14 12:14:59 -0700

    album and photo view will always receive an id; convert strings to ints

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -3,19 +3,16 @@ from .models import Album, Photo, PUBLIC
 
 # Create your views here.
 
+
 def latest_library_view(request, **kwargs):
     return render(request, "library_view.html", {"photos": Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")[:100]})
 
+
 def album_view(request, album_id=None, **kwargs):
-    if album_id:
-        photos = get_object_or_404(Album, id=album_id).photos.all()
-    else:
-        return redirect('library')
+    photos = get_object_or_404(Album, id=int(album_id)).photos.all()
     return render(request, "library_view.html", {"photos": photos})
 
+
 def photo_view(request, photo_id=None, **kwrags):
-    if photo_id:
-        photo = get_object_or_404(Photo, id=photo_id)
-    else:
-        return redirect('library')
+    photo = get_object_or_404(Photo, id=int(photo_id))
     return render(request, "photo_view.html", {"photo": photo})
