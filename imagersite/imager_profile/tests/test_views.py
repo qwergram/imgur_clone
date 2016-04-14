dc26429d4520047f29436fdf3ef5828a0772a818
Norton Pengra <npengra317@gmail.com>
2016-04-13 19:11:02 -0700

    Fixed syntax error

--- imagersite/imager_profile/tests/test_views.py
+++ imagersite/imager_profile/tests/test_views.py
@@ -284,7 +284,7 @@ class ProfileViewTestCase(TestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_other_profile_exists(self):
-        self.assertEqual(self.)
+        self.assertEqual(self.response2.status_code, 200)
 
     def test_profile_url_is_correct(self):
         self.assertTrue("<h1>YOU ARE AT PROFILES</h1>" in self.response.content.decode())
