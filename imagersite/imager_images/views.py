9dda4ba15ba726d5257c781b64f39fc32d234540
Norton Pengra <npengra317@gmail.com>
2016-04-13 20:04:00 -0700

    Added context for view

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Album, Photo
 
 # Create your views here.
 
 def latest_library_view(request, **kwargs):
-    return render(request, "library_view.html", {})
+    return render(request, "library_view.html", {"photos": Photo.objects.order_by("-date_uploaded")[:100]})
 
 def album_view(request, album_id=None, **kwargs):
     pass
