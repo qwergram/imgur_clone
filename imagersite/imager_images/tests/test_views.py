c6f5cd8ba5cc4bf103bcea179a10272623f192bf
Norton Pengra <npengra317@gmail.com>
2016-04-13 19:52:42 -0700

    New test for library_view

--- imagersite/imager_images/tests/test_views.py
+++ imagersite/imager_images/tests/test_views.py
@@ -10,8 +10,10 @@ class LibraryImageTestCase(TestCase):
         self.response = self.client.get('/images/library/')
 
     def test_view_exists(self):
-        self.assertTrue(self.response.status_code, 200)
+        self.assertTrue(self.response.status_code == 200)
 
+    def test_view_is_correct(self):
+        self.assertTrue("<h1>YOU ARE AT LIBRARY VIEW!</h1>" in self.response.content.decode())
 
 class AlbumViewTestCase(TestCase):
 
