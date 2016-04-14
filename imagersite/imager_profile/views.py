f669e1812db9dfa903af7d39572df0b05c4b1564
Norton Pengra <npengra317@gmail.com>
2016-04-13 18:37:41 -0700

    Fixed no photo edge case

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -8,6 +8,11 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self):
+        try:
+            photo = Photo.objects.filter(published=PUBLIC).order_by("?")[0]
+        except IndexError:
+            photo = None
+
         return {
-            "random_photo": Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded").order_by("?")[0:1]
+            "random_photo": photo
         }
