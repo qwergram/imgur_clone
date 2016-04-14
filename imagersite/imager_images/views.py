77b3eb327384c402b5c561d48712bfbfa33edb1b
Norton Pengra <npengra317@gmail.com>
2016-04-13 20:25:16 -0700

    Created linking for images

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -7,7 +7,7 @@ def latest_library_view(request, **kwargs):
     return render(request, "library_view.html", {"photos": Photo.objects.order_by("-date_uploaded")[:100]})
 
 def album_view(request, album_id=None, **kwargs):
-    pass
+    return render(request, "album_base.html", {})
 
 def photo_view(request, photo_id=None, **kwrags):
-    pass
+    return render(request, "album_base.html", {})
