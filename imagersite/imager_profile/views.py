8d9f2357f9f5cc5944c3f74a87d49bc73b8019ec
Norton Pengra <npengra317@gmail.com>
2016-04-13 18:27:51 -0700

    Added photo to homepage

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from imager_images.models import Photo
 
 # Create your views here.
 
@@ -7,4 +8,6 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self):
-        return {}
+        return {
+            random_photo: Photo.objects.filter(published=Photo.PUBLIC).order_by("-date_uploaded")[:100].order_by("?")[0]
+        }
