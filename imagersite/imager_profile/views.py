79098ad7057b8b246cbef15551e0992d97810f76
Norton Pengra <npengra317@gmail.com>
2016-04-18 23:21:00 -0700

    New profile_edit view

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .models import ImagerProfile
+from .forms import EditProfile
 from imager_images.models import Photo, PUBLIC
 
 # Create your views here.
@@ -27,3 +29,23 @@ def profile_view(request, profile_id=None, **kwargs):
         profile = get_object_or_404(ImagerProfile, id=int(profile_id))
 
     return render(request, "profile.html", context={"profile": profile})
+
+
+def profile_edit(request, *args, **kwargs):
+    profile = request.user.profile
+    if request.method == "POST":
+        form = EditProfile(request.POST)
+        if form.is_valid():
+            return HttpResponse("POST method!")
+
+        return HttpResponse("Invalid!")
+    else:
+        return render(request, "edit_profile.html", {"form": EditProfile(initial={
+            "camera": profile.camera,
+            "personality_type": profile.personality_type,
+            "category": profile.category,
+            "github": profile.github,
+            "first_name": profile.user.first_name,
+            "last_name": profile.user.last_name,
+            "email": profile.user.email,
+        })})
