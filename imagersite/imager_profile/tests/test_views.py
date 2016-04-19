89c59beb61820a2738f1c1d252166e1f7896c244
Norton Pengra <npengra317@gmail.com>
2016-04-19 15:12:44 -0700

    Updated tests to work with new templates

--- imagersite/imager_profile/tests/test_views.py
+++ imagersite/imager_profile/tests/test_views.py
@@ -72,13 +72,13 @@ class OwnProfileViewTestCase(TestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_logout_button_appears(self):
-        self.assertContains(self.response, ' class="button big fit">Log Out</a></li>')
+        self.assertContains(self.response, ' class="button">Log Out</a></li>')
 
     def test_login_doesnt_exist(self):
-        self.assertNotContains(self.response, ' class="button big fit">Log In</a></li>')
+        self.assertNotContains(self.response, ' class="button">Log In</a></li>')
 
     def test_register_doesnt_exist(self):
-        self.assertNotContains(self.response, ' class="button big fit">Register</a></li>')
+        self.assertNotContains(self.response, ' class="button">Register</a></li>')
 
     def test_username_appears(self):
         self.assertContains(self.response, self.user.username)
@@ -221,10 +221,10 @@ class IndexPageDefaultViewTestCase(TestCase):
         self.assertNotContains(self.response, '<p>Hello World!</p>')
 
     def test_login_button_appears(self):
-        self.assertContains(self.response, 'class="button big fit">Log In</a>')
+        self.assertContains(self.response, 'class="button">Log In</a>')
 
     def test_register_button_appears(self):
-        self.assertContains(self.response, ' class="button big fit">Register</a></li>')
+        self.assertContains(self.response, ' class="button">Register</a></li>')
 
     def test_username_filler_appears(self):
         self.assertContains(self.response, 'Imgur Clone')
