c14147c7bb727865e8aba8531eed2c09505243c1
Norton Pengra <npengra317@gmail.com>
2016-04-18 19:30:34 -0700

    New form for image uploads

---
+++ imagersite/imager_images/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import PRIVACY_CHOICES
+
+class NewImage(forms.Form):
+    title = forms.CharField(max_length=255)
+    description = forms.CharField()
+    published = forms.ChoiceField(choices=PRIVACY_CHOICES)
+    photo = forms.ImageField()
