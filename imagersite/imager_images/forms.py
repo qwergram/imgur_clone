26f41e50b2f12005a8bae0f6e656c4ab34b549eb
Norton Pengra <npengra317@gmail.com>
2016-04-18 20:27:21 -0700

    New Album form

--- imagersite/imager_images/forms.py
+++ imagersite/imager_images/forms.py
@@ -7,3 +7,10 @@ class NewImage(forms.Form):
     description = forms.CharField()
     published = forms.ChoiceField(choices=PRIVACY_CHOICES)
     photo = forms.ImageField()
+
+
+class NewAlbum(forms.Form):
+    title = forms.CharField(max_length=255)
+    description = forms.CharField()
+    published = forms.ChoiceField(choices=PRIVACY_CHOICES)
+    photo = forms.MultipleChoiceField()
