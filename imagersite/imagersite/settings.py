4680fa9420c79702179d4452238125563dd2145f
Norton Pengra <npengra317@gmail.com>
2016-04-28 10:13:03 -0700

    New media_url

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/'
 
 MEDIA_ROOT = './'
 STATIC_ROOT = './static/'
