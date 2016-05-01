3cc655b911796ab807ada4ba7a97255f2c6093b9
Kent Ross <root.main@gmail.com>
2016-04-30 23:45:32 -0700

    attached published date semantics to albums too

--- imagersite/imager_images/handlers.py
+++ imagersite/imager_images/handlers.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 from django.db.models.signals import pre_save
-from imager_images.models import Photo, PUBLIC
+from imager_images.models import Photo, Album, PUBLIC
 from django.dispatch import receiver
 from datetime import datetime
 
 
 @receiver(pre_save, sender=Photo)
+@receiver(pre_save, sender=Album)
 def set_published_date(sender, instance=None, **kwargs):
     """Set the date_published field to the appropriate value based on its
     current value and the value of the PUBLISHED field"""
